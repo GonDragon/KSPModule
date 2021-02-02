9a3328e19bec88eb863b83589092c02e179f28ca
Gonzalo Romero <gondragon13@gmail.com>
2021-02-02 16:13:08 -0300

    More code for modules

--- KSPModule/Module.py
+++ KSPModule/Module.py
@@ -6,6 +6,35 @@ class Module:
 
     def __init__(self, type: str = 'MODULE'):
         self.type = type
+        self.content = []
+
+    def add_attribute(self, key: str, value: str):
+        """
+        Add a new attribute to the module. The attribute must be
+        a pair key-value of strings.
+        One Module can host multiple attributes with the same name.
+        """
+        self.content.append(Attribute(key, value))
+
+    def get_attribute(self, key: str, n: int = 1):
+        """
+        Return the value of an Attribute with certain key. If there
+        are multiple attributes with the same key, you can specify,
+        in order, wich one do you want (n).
+        By default, it returns the first one (n=1).
+        """
+        i = 1
+        for obj in self.content:
+            if type(obj) is Attribute and obj.key == key:
+                if n == i:
+                    return obj.value
+                i += 1
+
+        if i > 1:
+            raise IndexError(
+                'The attribute "{0}" does not have that many occurrences.'.format(key))
+        raise KeyError(
+            'The module does not contain the attribute "{0}".'.format(key))
 
 
 class Attribute:
--- tests/Module_test.py
+++ tests/Module_test.py
@@ -6,7 +6,28 @@ from KSPModule.Module import Module
 class ModuleTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.empty_module = Module()
 
-    def test_tre(self):
-        self.assertEqual(True, True)
+    def test_save_attribute(self):
+        self.empty_module.add_attribute('foo', 'bar')
+
+        self.assertEqual(self.empty_module.get_attribute('foo'), 'bar')
+
+    def test_save_multiple_attribute(self):
+        self.empty_module.add_attribute('foo', 'bar')
+        self.empty_module.add_attribute('foo', 'bar')
+        self.empty_module.add_attribute('foo', 'bar')
+        self.empty_module.add_attribute('foo', 'bar')
+        self.empty_module.add_attribute('bar', 'bar')
+
+        self.assertEqual(self.empty_module.get_attribute('bar'), 'bar')
+
+    def test_save_multiple_attribute_index(self):
+        self.empty_module.add_attribute('foo', '1')
+        self.empty_module.add_attribute('foo', '2')
+        self.empty_module.add_attribute('bar', 'bar')
+        self.empty_module.add_attribute('foo', '3')
+        self.empty_module.add_attribute('foo', '4')
+        self.empty_module.add_attribute('bar', 'bar')
+
+        self.assertEqual(self.empty_module.get_attribute('foo', 3), '3')
