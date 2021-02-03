faf97b69747e8735d216fe6359894b05ec1fa700
Gonzalo Romero <gondragon13@gmail.com>
2021-02-03 19:40:40 -0300

    Fixed Parser and finished tests

--- KSPModule/Parser.py
+++ KSPModule/Parser.py
@@ -1,4 +1,28 @@
 import re
+from KSPModule.Module import Module
+
+# This case match closing modules without modules inside.
+# Group 1: Attributes of the current Module
+# Group 2: Continue of the rawstring outside the current Module
+REG_CLOSE = re.compile(
+    r'([^\{\}]*)\}\s*(.*)', flags=re.DOTALL)
+
+# This case match opening modules
+# Group 1: Attributes of the current Module
+# Group 2: Type of a new opening module
+# Group 3: Rawstring beyond the new opening module
+REG_OPEN = re.compile(
+    r'((?:\s*\w*\s*=\s*\w*)*)\s*([A-Z]+)\s*\{\s*(.*)', flags=re.DOTALL)
+
+# This case match attributes
+# Group 1: Key
+# Group 2: Value
+REG_ATTR = re.compile(
+    r'(\w+)\s*=\s*(\w+)', flags=re.DOTALL)
+
+# This case match emptiness
+REG_EMPTY = re.compile(
+    r'^\s*$', flags=re.DOTALL)
 
 
 class Parser:
@@ -17,3 +41,39 @@ class Parser:
         while '' in lines:
             lines.remove('')
         self.raw = '\n'.join(lines)
+
+    def _get_content(self, module: Module, raw: str):
+
+        raw_attributes = ''
+        current_raw = raw
+
+        while not re.match(REG_CLOSE, current_raw):
+            if re.match(REG_EMPTY, current_raw):
+                return ''
+
+            more_attr, module_type, current_raw = re.match(
+                REG_OPEN, current_raw).groups()
+            raw_attributes += more_attr
+            new_module = Module(module_type)
+            current_raw = self._get_content(new_module, current_raw)
+            module.add_module(new_module)
+
+        last_attr, raw_continue = re.match(REG_CLOSE, current_raw).groups()
+        raw_attributes += last_attr
+
+        for k, v in self._attr_from_raw(raw_attributes):
+            module.add_attribute(k, v)
+
+        return raw_continue
+
+    def _attr_from_raw(self, rawattr: str):
+
+        return re.findall(REG_ATTR, rawattr)
+
+    def get_modules(self):
+        """
+        Returns a list containing all the base modules.
+        """
+        module_container = Module()
+        self._get_content(module_container, self.raw)
+        return module_container.get_modules()
--- tests/Parser_test.py
+++ tests/Parser_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from KSPModule.Parser import Parser
+from KSPModule.Module import Module
 
 
 class ParserTestCase(unittest.TestCase):
@@ -13,3 +14,41 @@ class ParserTestCase(unittest.TestCase):
         commented = self.loadCFG('simple')
         uncommented = self.loadCFG('simple_commented')
         self.assertEqual(commented.raw, uncommented.raw)
+
+    def test_create_one_module(self):
+        simple = self.loadCFG('simple')
+        modules = simple.get_modules()
+        self.assertEqual(len(modules), 1)
+        self.assertIsInstance(modules[0], Module)
+
+    def test_create_multiple_modules(self):
+        multiple = self.loadCFG('multiple')
+        modules = multiple.get_modules()
+        self.assertEqual(len(modules), 3)
+        for module in modules:
+            self.assertIsInstance(module, Module)
+
+    def test_create_one_module_nested(self):
+        nested = self.loadCFG('nested')
+        modules = nested.get_modules()
+        self.assertEqual(len(modules), 1)
+        self.assertIsInstance(modules[0], Module)
+
+    def test_create_multiple_modules_nested(self):
+        multiple = self.loadCFG('multiple_nested')
+        modules = multiple.get_modules()
+        self.assertEqual(len(modules), 3)
+        for module in modules:
+            self.assertIsInstance(module, Module)
+
+    def test_create_one_module_nested_without_spacing(self):
+        nested = self.loadCFG('nested_without_spacing')
+        modules = nested.get_modules()
+        self.assertEqual(len(modules), 1)
+        self.assertIsInstance(modules[0], Module)
+
+    def test_create_one_module_nested_double(self):
+        nested = self.loadCFG('nested_double')
+        modules = nested.get_modules()
+        self.assertEqual(len(modules), 1)
+        self.assertIsInstance(modules[0], Module)
