736def2c65a18c9df61536c51f5d04a909468797
Gonzalo Romero <gondragon13@gmail.com>
2021-02-10 01:17:39 -0300

    fix a problem with distribution setup

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 # setup.py
-from setuptools import setup, find_packages
+from setuptools import setup
 
 # To use a consistent encoding
-from codecs import open
+from io import open
 from os import path
 
 # The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+with open(path.join(HERE, 'README.md'), encoding='utf-8-sig') as f:
     long_description = f.read()
 
 setup(
     name="KSPModule",
     version="0.1.0",
     description="Library to parse KSP CFG files.",
-    long_description=long_description,
     long_description_content_type="text/markdown",
+    long_description=long_description,
     author="Gonzalo Romero",
     author_email="romero.gonzalo.h@gmail.com",
     license="MIT",
