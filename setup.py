bbd16dd6920eb0f3f7bd1f16e0181ab6bf6172ea
Gonzalo Romero <gondragon13@gmail.com>
2021-02-10 00:49:04 -0300

    prepare for distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,35 @@
 # setup.py
-from setuptools import setup
-setup(setup_requires=["pbr"],
-      pbr=True)
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="KSPModule",
+    version="0.1.0",
+    description="Library to parse KSP CFG files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Gonzalo Romero",
+    author_email="romero.gonzalo.h@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["KSPModule"],
+    include_package_data=True,
+)
